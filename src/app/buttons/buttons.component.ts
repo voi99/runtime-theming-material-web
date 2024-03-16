92f6da761fbed2fa5ef20fa6bb17a7f50058184e
voi99 <ivanivopop@gmail.com>
2024-03-16 18:50:55 +0100

    upd: source color picker

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -1,10 +1,11 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from "@angular/core";
 import "@material/web/button/elevated-button.js";
 import "@material/web/button/filled-button.js";
 import "@material/web/button/filled-tonal-button.js";
 import "@material/web/button/outlined-button.js";
 import "@material/web/button/text-button.js";
 import "@material/web/icon/icon.js";
+
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from "@angular/core";
 import { SectionComponent } from "../section/section.component";
 
 @Component({
@@ -12,11 +13,11 @@ import { SectionComponent } from "../section/section.component";
   standalone: true,
   template: `<app-section title="Buttons">
     <div class="flex-wrapper">
-      <md-filled-button> Filled </md-filled-button>
-      <md-outlined-button> Outlined </md-outlined-button>
-      <md-elevated-button> Elevated </md-elevated-button>
+      <md-filled-button>Filled</md-filled-button>
+      <md-outlined-button>Outlined</md-outlined-button>
+      <md-elevated-button>Elevated</md-elevated-button>
       <md-filled-tonal-button>Tonal</md-filled-tonal-button>
-      <md-text-button> Text </md-text-button>
+      <md-text-button>Text</md-text-button>
       <md-filled-tonal-button>
         Send
         <svg slot="icon" viewBox="0 0 48 48">
--- src/app/theme/theme.component.ts
+++ src/app/theme/theme.component.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, computed, effect, signal } from "@angular/core";
+import { CUSTOM_ELEMENTS_SCHEMA, Component, computed, effect, inject, signal } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { applyTheme, argbFromHex, themeFromSourceColor } from "@material/material-color-utilities";
 import { DEFAULT_COLOR } from "./default-theme";
@@ -6,42 +6,93 @@ import { LS_IS_DARK_MODE_KEY, LS_SOURCE_COLOR_KEY, getSavedColor, getSavedThemeM
 import { SectionComponent } from "../section/section.component";
 import "@material/web/icon/icon.js";
 import "@material/web/iconbutton/outlined-icon-button.js";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "app-theme",
   template: `<app-section title="Theme Controls">
-    <input type="color" [ngModel]="color()" (ngModelChange)="color.set($event)" />
+    <div class="theme-controls">
+      <div class="source-color-picker">
+        <label for="color-input" class="color-input-label">
+          <span> Source Color </span>
+          <span class="color-input-wrapper">
+            <div class="overflow">
+              <input id="color-input" type="color" [ngModel]="color()" (ngModelChange)="color.set($event)" />
+            </div>
+          </span>
+        </label>
+      </div>
 
-    <md-outlined-icon-button (click)="toggleMode()">
-      <md-icon>
-        @if (isDarkMode()) {
-          dark_mode
-        } @else {
-          light_mode
-        }
-      </md-icon>
-    </md-outlined-icon-button>
+      <md-outlined-icon-button (click)="toggleMode()">
+        <md-icon>
+          @if (isDarkMode()) {
+            dark_mode
+          } @else {
+            light_mode
+          }
+        </md-icon>
+      </md-outlined-icon-button>
+    </div>
   </app-section> `,
   standalone: true,
+  styles: `
+    .theme-controls {
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+
+      .source-color-picker {
+        padding: 0.5rem;
+        border-radius: 12px;
+        background-color: var(--md-sys-color-surface-variant);
+        color: var(--md-sys-color-on-surface-variant);
+
+        .color-input-label {
+          display: flex;
+          align-items: center;
+          gap: 0.75rem;
+
+          .color-input-wrapper {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            box-sizing: border-box;
+            border: 1px solid var(--md-sys-color-on-secondary-container);
+            position: relative;
+
+            .overflow {
+              width: 100%;
+              height: 100%;
+              overflow: hidden;
+              border-radius: inherit;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+
+              #color-input {
+                border: none;
+                background: none;
+                min-width: 150%;
+                min-height: 150%;
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
   imports: [FormsModule, SectionComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ThemeComponent {
+  #document = inject(DOCUMENT);
+
   color = signal(getSavedColor() || DEFAULT_COLOR);
   isDarkMode = signal<boolean>(getSavedThemeMode());
-
-  theme = computed(() =>
-    themeFromSourceColor(argbFromHex(this.color()), [
-      {
-        name: "custom-1",
-        value: argbFromHex(this.color()),
-        blend: true,
-      },
-    ])
-  );
+  theme = computed(() => themeFromSourceColor(argbFromHex(this.color())));
 
   applyThemeEffect = effect(() => {
-    applyTheme(this.theme(), { target: document.body, dark: this.isDarkMode() });
+    applyTheme(this.theme(), { target: this.#document.body, dark: this.isDarkMode() });
   });
 
   saveSourceColorEffect = effect(() => {
